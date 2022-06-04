41157a4052036a12644868363d4904c7d06e2614
Pedro Ruelas <pruelas5238@yahoo.com>
2022-06-03 19:43:35 -0700

    Created base functions and respective CSS for adding and removing books

--- odin-library.js
+++ odin-library.js
@@ -12,6 +12,18 @@ function Book(title, author, pages, read){
     }
 }
 
+function removeBook(book){
+    console.log(`removed ${book.getAttribute('data-index')}`);
+    console.log(book.getAttribute('data-index'), book.getAttribute('data-index')+1)
+    delete myLibrary[(book.getAttribute('data-index'))];
+    console.log(myLibrary);
+    addBookToLibrary();
+}
+
+function addBook(){
+    console.log("addBook");
+}
+
 let book1 = new Book(1, 1, 1, 1);
 let book2 = new Book(2, 2, 2, 2);
 let book3 = new Book(3, 3, 3, 3);
@@ -19,35 +31,44 @@ let book3 = new Book(3, 3, 3, 3);
 let myLibrary = [book1, book2, book3];
 
 function addBookToLibrary(){
+    console.log("ran");
     var libraryDisplay = document.getElementById("libraryDisplay");
     var bookDiv;
     var bookTitle;
     var bookAuthor;
     var bookPages;
     var bookRead;
+    var bookRemove;
     for(var i = 0; i< myLibrary.length; i++){
+        if(typeof myLibrary[i] !== 'undefined' ){
+            bookDiv = document.createElement("div");
+            bookTitle = document.createElement("div");
+            bookAuthor = document.createElement("div");
+            bookPages = document.createElement("div");
+            bookRead = document.createElement("div");
+            bookRemove = document.createElement("button");
 
-        bookDiv = document.createElement("div");
-        bookTitle = document.createElement("div");
-        bookAuthor = document.createElement("div");
-        bookPages = document.createElement("div");
-        bookRead = document.createElement("div");
+            bookDiv.className = "book";
+            bookTitle.className = "title";
+            bookAuthor.className = "author";
+            bookPages.className = "pages";
+            bookRead.className = "read";
+            bookRemove.className = "bookRemove"
 
-        bookDiv.className = "book";
-        bookTitle.className = "title";
-        bookAuthor.className = "author";
-        bookPages.className = "pages";
-        bookRead.className = "read";
+            bookTitle.textContent = myLibrary[i].title;
+            bookAuthor.textContent = myLibrary[i].author;
+            bookPages.textContent = myLibrary[i].pages;
+            bookRead.textContent = myLibrary[i].read;
+            bookRemove.textContent = "X";
+            bookRemove.setAttribute('data-index', i);
+            console.log(bookRemove.getAttribute('data-index'));
+            bookRemove.onclick = function() { removeBook(this); };
 
-        bookTitle.textContent = myLibrary[i].title;
-        bookAuthor.textContent = myLibrary[i].author;
-        bookPages.textContent = myLibrary[i].pages;
-        bookRead.textContent = myLibrary[i].read;
+            bookDiv.append(bookRemove, bookTitle, bookAuthor, bookPages, bookRead,);
 
-        bookDiv.append(bookTitle, bookAuthor, bookPages, bookRead);
-
-        libraryDisplay.appendChild(bookDiv);
-    }
+            libraryDisplay.appendChild(bookDiv);
+        };
+    };
 
 }
 

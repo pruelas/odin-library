ffb7f31bb0e8848295949fd5fc8a6fd98254d743
Pedro Ruelas <pruelas5238@yahoo.com>
2022-06-07 19:41:36 -0700

    Added a toggle button to change a book's read status

--- odin-library.js
+++ odin-library.js
@@ -3,37 +3,40 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        if(read){
-            return (title + " by " + author + ", " + pages + " pages, " + "has read already");
-        }else{
-            return (title + " by " + author + ", " + pages + " pages, " + "not ready yet");
-        }
-    }
 }
 
 function removeBook(book){
-    console.log(`removed ${book.getAttribute('data-index')}`);
-    console.log(book.getAttribute('data-index'), book.getAttribute('data-index')+1)
     delete myLibrary[(book.getAttribute('data-index'))];
-    console.log(myLibrary);
     addBookToLibrary();
 }
 
 function addBookForm(){
-    console.log("addBook");
     var newBookForm = document.getElementById("newBookForm");
     newBookForm.style.visibility = "visible";
 }
 
+function toggleHasRead(book){
+    let currentReadStatus = myLibrary[book.getAttribute('data-index')].read;
+    if(currentReadStatus === "true"){
+        book.textContent = "No";
+        book.className = "hasNotRead";
+        myLibrary[book.getAttribute('data-index')].read = "false";
+
+    }else{
+        book.textContent = "Yes";
+        book.className ="hasRead";
+        myLibrary[book.getAttribute('data-index')].read = "true";
+    }
+
+}
+
 let book1 = new Book(1, 1, 1, 1);
 let book2 = new Book(2, 2, 2, 2);
-let book3 = new Book(3, 3, 3, 3);
+let book3 = new Book(3, 3, 3, "true");
 
 let myLibrary = [book1, book2, book3];
 
 function addBookToLibrary(){
-    console.log("ran");
     var libraryDisplay = document.getElementById("libraryDisplay");
     libraryDisplay.innerHTML = "";
     var bookDiv;
@@ -42,6 +45,7 @@ function addBookToLibrary(){
     var bookPages;
     var bookRead;
     var bookRemove;
+    var hasReadButton;
     for(var i = 0; i< myLibrary.length; i++){
         if(typeof myLibrary[i] !== 'undefined' ){
             bookDiv = document.createElement("div");
@@ -49,6 +53,7 @@ function addBookToLibrary(){
             bookAuthor = document.createElement("div");
             bookPages = document.createElement("div");
             bookRead = document.createElement("div");
+            hasReadButton = document.createElement("button");
             bookRemove = document.createElement("button");
 
             bookDiv.className = "book";
@@ -61,26 +66,35 @@ function addBookToLibrary(){
             bookTitle.textContent = myLibrary[i].title;
             bookAuthor.textContent = myLibrary[i].author;
             bookPages.textContent = myLibrary[i].pages;
-            bookRead.textContent = myLibrary[i].read;
+
+            bookRead.textContent = "Has Read: "
+            hasReadButton.setAttribute('data-index', i);
+            hasReadButton.onclick = function(){ toggleHasRead(this);};
+
+            if(myLibrary[i].read === "true"){
+                hasReadButton.textContent = "Yes";
+                hasReadButton.className = "hasRead";
+
+            }else{
+                hasReadButton.textContent = "No";
+                hasReadButton.className ="hasNotRead";
+            }
+
             bookRemove.textContent = "X";
             bookRemove.setAttribute('data-index', i);
-            console.log(bookRemove.getAttribute('data-index'));
             bookRemove.onclick = function() { removeBook(this); };
 
-            bookDiv.append(bookRemove, bookTitle, bookAuthor, bookPages, bookRead);
+            bookDiv.append(bookRemove, bookTitle, bookAuthor, bookPages, bookRead, hasReadButton);
 
             libraryDisplay.appendChild(bookDiv);
         };
     };
 
 }
-
-console.log("we running?" + myLibrary.length);
 addBookToLibrary();
 
 document.body.addEventListener('click', function(e){
     var addBookForm = document.getElementById("newBookForm");
-    console.log(addBookForm.style.visibility + "eventListener" + e.target.className + e.target);
     if( (e.target.className !=="addBookForm" && e.target.className !=="hasRead" && e.target.className !=="addBook" && e.target instanceof HTMLDivElement) && addBookForm.style.visibility !=="hidden"){
         addBookForm.style.visibility = 'hidden';
     }
@@ -88,7 +102,6 @@ document.body.addEventListener('click', function(e){
 
 const addBook = document.getElementById("addBookForm");
 addBook.addEventListener('submit', function(e){
-    console.log(addBook.elements['title'].value, addBook.elements['read'].value, addBook.elements[3].value);
     document.getElementById("newBookForm").style.visibility = "hidden";
     myLibrary.push(new Book(addBook.elements['title'].value, addBook.elements['author'].value, addBook.elements['pages'].value, addBook.elements['read'].value));
     addBookToLibrary();
